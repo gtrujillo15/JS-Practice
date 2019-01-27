f582f10caeb740fcb579b2313dca6cfffd8cb795
Gabriela T <gtrujillo15@gmail.com>
2019-01-26 17:23:18 -0800

    created boilerplate and added some console logs

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+console.log('hello world');
+console.log("My name is Gaby and I'm 26 years old.");
+console.log("I enjoy learning, helping others, and sometimes baking");
+console.log("I hope I can use this repo a lot to help me learn JS");
