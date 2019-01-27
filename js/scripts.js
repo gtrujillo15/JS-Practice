d489fb2afc4b69fe34643783de6292e045e67251
Gabriela T <gtrujillo15@gmail.com>
2019-01-26 21:01:11 -0800

    practicing calls and objects day 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
+// Day One
 console.log('hello world');
 console.log("My name is Gaby and I'm 26 years old.");
 console.log("I enjoy learning, helping others, and sometimes baking");
 console.log("I hope I can use this repo a lot to help me learn JS");
+
+// Day Two
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+console.log(lastName);
+alert(lastName);
+var birthday = new Date();
+var birthday = prompt('When is your birthday?');
+confirm(birthday);
+alert(birthday);
