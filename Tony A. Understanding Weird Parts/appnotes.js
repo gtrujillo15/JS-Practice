01571c57cda081eee55d0a7245686aaf16b33fa8
Gabriela T <gtrujillo15@gmail.com>
2019-03-03 22:10:30 -0800

    added notes and practice examples from tony a, understanding the weird parts of js

---
+++ Tony A. Understanding Weird Parts/appnotes.js
@@ -0,0 +1,19 @@
+/* SYNTAX PARSER: A program that reads your code and determines what it edoes and if its grammar is valid. Any program was created to help the computer understand what you want to do. */
+
+/* People use compilers that are like translators for the computer and you to understand each other. */
+
+/* LEXICAL ENVIRONMENT: Where something sits physically in the code you write. Lexical means having to do with words or grammar. A lexical environment exists in programming languages in which WHERE you write something is important. */
+
+/* EXECUTION CONTEXT: A wrapper to help manage the code that is running. There are lots of lexical environments. Which one is currently running is managed via execution contexts. It can contain things beyond what you've written in your code */
+
+/* NAME VALUE PAIR
+Name = Value
+Address = '100 Fair St'
+*/
+
+/* OBJECT: A collection of name value pairs*/
+
+// Code with Mosh - Javascript beginners, advanced, object oriented 
+// Andrew Mead - JS Bootcamp
+
+// INVOCATION: Running a function. In JS, by using parenthesis ()
