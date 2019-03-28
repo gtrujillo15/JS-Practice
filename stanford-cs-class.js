ad1ee4af9dcd558a6d949eb01af7ed1004dc8886
Gabriela T <gtrujillo15@gmail.com>
2019-03-27 22:18:29 -0700

    began the standcord cs class and taking notes

---
+++ stanford-cs-class.js
@@ -0,0 +1,15 @@
+//we can consider code to be a series of instructions
+
+//programmers try to think of how to come up with solutions to problems by trying to find algorithms
+
+// print 
+print(6);
+
+//print srings
+print('hello', 2, 'bye');
+
+//string is sequence of letters written within quotes to be used as data within the code 
+
+//syntax is code text structured for the computer and syntax errors are very common.
+
+//syntax errors are very common and all programmers make them all the time
